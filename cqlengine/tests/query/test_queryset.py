6782e0b9af2d8b4288d88eb80cc0b752bc74012c
Blake Eggleston <bdeggleston@gmail.com>
2013-06-26 08:10:19 -0700

    fixing query generation tests

--- cqlengine/tests/query/test_queryset.py
+++ cqlengine/tests/query/test_queryset.py
@@ -96,12 +96,12 @@ class TestQuerySetOperation(BaseCassEngTestCase):
         query1 = TestModel.objects(test_id=5)
         ids = [o.query_value.identifier for o in query1._where]
         where = query1._where_clause()
-        assert where == '"test_id" = :{}'.format(*ids)
+        assert where == '"test_id" = %({})s'.format(*ids)
 
         query2 = query1.filter(expected_result__gte=1)
         ids = [o.query_value.identifier for o in query2._where]
         where = query2._where_clause()
-        assert where == '"test_id" = :{} AND "expected_result" >= :{}'.format(*ids)
+        assert where == '"test_id" = %({})s AND "expected_result" >= %({})s'.format(*ids)
 
     def test_query_expression_where_clause_generation(self):
         """
@@ -110,12 +110,12 @@ class TestQuerySetOperation(BaseCassEngTestCase):
         query1 = TestModel.objects(TestModel.test_id == 5)
         ids = [o.query_value.identifier for o in query1._where]
         where = query1._where_clause()
-        assert where == '"test_id" = :{}'.format(*ids)
+        assert where == '"test_id" = %({})s'.format(*ids)
 
         query2 = query1.filter(TestModel.expected_result >= 1)
         ids = [o.query_value.identifier for o in query2._where]
         where = query2._where_clause()
-        assert where == '"test_id" = :{} AND "expected_result" >= :{}'.format(*ids)
+        assert where == '"test_id" = %({})s AND "expected_result" >= %({})s'.format(*ids)
 
     def test_querystring_generation(self):
         """

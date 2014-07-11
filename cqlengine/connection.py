76d73ee27e82a19e6be6eb781dc7568e0c8ab7ba
Michael Cyrulnik <lifto@mac.com>
2014-07-11 14:00:27 -0400

    connection speedup flags in setup.

--- cqlengine/connection.py
+++ cqlengine/connection.py
@@ -4,6 +4,7 @@
 
 from collections import namedtuple
 from cassandra.cluster import Cluster
+from cassandra.policies import HostDistance
 from cassandra.query import SimpleStatement, Statement
 
 try:
@@ -35,6 +36,8 @@ def setup(
         default_keyspace,
         consistency=ConsistencyLevel.ONE,
         lazy_connect=False,
+        skip_schema_loading=False,
+        one_core_connection=False,
         **kwargs):
     """
     Records the hosts and connects to one of them
@@ -47,6 +50,10 @@ def setup(
     :type consistency: int
     :param lazy_connect: True if should not connect until first use
     :type lazy_connect: bool
+    :param skip_schema_loading: True if should not load schema after connect. Will make connects faster. Will break prepared statements, and if you make a schema change, you won't properly wait for schema agreement.
+    :type skip_schema_loading: bool
+    :param one_core_connection: True if only one connection per host instead of 2
+    :type one_core_connection: bool
     """
     global cluster, session, default_consistency_level, lazy_connect_args
 
@@ -65,6 +72,10 @@ def setup(
         return
 
     cluster = Cluster(hosts, **kwargs)
+    if skip_schema_loading:
+        cluster.control_connection._refresh_schema = lambda *args, **kwargs: None
+    if one_core_connection:
+        cluster.set_core_connections_per_host(HostDistance.LOCAL, 1)
     session = cluster.connect()
     session.row_factory = dict_factory
 
